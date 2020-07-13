2943134b356611b1df1718dd6f12af85e8e5c1da
YusukeHirose <kodecurry@gmail.com>
2020-07-13 20:26:39 +0900

    feat: header実装

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,19 @@ import React from 'react'
 const Header = () => {
     return (
         <>
-            <div>
-                header
-            </div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand col-md-3" href="localhost:3000">Movie</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse col-md-5 offset-md-5" id="navbarSupportedContent">
+                    <form className="form-inline">
+                        <input className="form-control mr-sm-2" id="searchForm" type="search" placeholder="Search" aria-label="Search"></input>
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
         </>
     )
 }
