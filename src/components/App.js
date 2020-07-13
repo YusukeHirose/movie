19e74077b5a94009c01de8ae63b23a069a17e7d2
YusukeHirose <kodecurry@gmail.com>
2020-07-13 21:33:52 +0900

    feat: contentsコンポーネント作成

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from './Header';
+import Contents from './Contents'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Contents />
     </div>
   );
 }
---
+++ src/components/Contents.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contents = () => {
+    return (
+        <>
+            <div>
+                Contents
+            </div>
+        </>
+    )
+}
+export default Contents
