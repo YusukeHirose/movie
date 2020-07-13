5768436f0a30e478a85db24a591811c05a9e788c
YusukeHirose <kodecurry@gmail.com>
2020-07-14 03:22:37 +0900

    feat: style調整

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,17 +3,30 @@ import React from 'react'
 const Content = ({content}) => {
     const wrapperStyle = {
         marginLeft:"calc(100vw - 98vw)",
-        marginBottom:"calc(100vh - 98vh)"
+        marginBottom:"calc(100vh - 98vh)",
+        height: "calc(100vh - 32vh)",
+    }
+    const cardStyle = {
+        width: "16rem",
+    }
+    const titleStyle = {
+        whiteSpace: "nowrap",
+        overflow: "hidden",
+        textOverflow: "ellipsis",
+    }
+
+    const imgStyle = {
+        height: "calc(100vh - 60vh)"
     }
 
     return (
         <>
             <div className="col-sm-2" style={wrapperStyle}>
-                <div className="card" style={{width: "16rem"}}>
-                    <img src="https://www.homepage-tukurikata.com/image/lion.jpg" className="card-img-top" alt="..."></img>
+                <div className="card" style={cardStyle}>
+                    <img src={content["poster_path"]} className="card-img-top" alt="..." style={imgStyle}></img>
                     <div className="card-body">
-                        <h5 className="card-title">{content["title"]}</h5>
-                        <h6 className="card-text">{content["release_date"]}</h6>
+                        <h5 className="card-title" style={titleStyle}>{content["title"]}</h5>
+                        <p className="card-text">Release: {content["release_date"]}</p>
                         <a href="localhost:3000" className="btn btn-primary">Go Detail</a>
                     </div>
                 </div>
