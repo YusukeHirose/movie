2fb47693f85b4883a5b8cb8ac138214982fc03bb
YusukeHirose <kodecurry@gmail.com>
2020-07-13 22:07:33 +0900

    feat: 固定値でcard作成

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,6 +4,14 @@ const Content = () => {
     return (
         <>
             <div>content</div>
+            <div className="card" style={{width: "18rem"}}>
+                <img src="https://www.homepage-tukurikata.com/image/lion.jpg" className="card-img-top" alt="..."></img>
+                <div className="card-body">
+                    <h5 className="card-title">Title</h5>
+                    <h6 className="card-text">ReleaseDate</h6>
+                    <a href="localhost:3000" className="btn btn-primary">Go Detail</a>
+                </div>
+            </div>
         </>
     )
 }
